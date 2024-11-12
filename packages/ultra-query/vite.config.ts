5cbf5f0b91672272c6559c331e02bab8806a2d39
lushevol <lushevol@outlook.com>
2024-11-12 16:03:48 +0800

    chore: lint

--- packages/ultra-query/vite.config.ts
+++ packages/ultra-query/vite.config.ts
@@ -1,4 +1,4 @@
-import * as path from 'path';
+import * as path from 'node:path';
 import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import react from '@vitejs/plugin-react';
